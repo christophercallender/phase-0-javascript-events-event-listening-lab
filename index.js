58db84a4a5b5ed6a0384e7bcd9cb3a9646004393
Christopher Callender <christophercallender0000@gmail.com>
2022-04-25 17:20:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+   const input = document.getElementById('input');
+
+   function clickAlert() {
+      alert('Hee hee, that tickles!');
+   }
+
+   input.addEventListener('click', clickAlert);
 }
+addingEventListener();
